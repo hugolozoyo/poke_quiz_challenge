00e488b6bc734449fff06b8db9f80ac64780df68
Hugo Belman <hugobelman@outlook.com>
2024-08-14 14:17:20 -0600

    Enable cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,15 +20,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
-  # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
-
-  # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
-
-  # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.cache_store = :file_store, 'tmp/cache'
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
--- config/initializers/cache_store.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.config.cache_store = :file_store, 'tmp/cache'
